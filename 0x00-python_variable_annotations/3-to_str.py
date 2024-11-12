0578474e6c82c1de9ca4e569f51566e27ba914ad
magdielian-code <oziomaagaecheta81@gmail.com>
2024-11-12 21:47:06 +0100

    task: Write a type-annotated function to_str that takes a float n as argument and returns the string representation of the float.

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""3. Basic annotations - to string"""
+
+def to_str(n:float) -> str:
+    """ function to_str which takes a float n as argument and returns the string representation of the float. """
+    return str(n)
