d39936f8a90666df47791a2ce1a451cc1292ca05
magdielian-code <oziomaagaecheta81@gmail.com>
2025-01-17 17:02:55 +0100

    corrected async functionproject

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,27 +1,21 @@
+
 #!/usr/bin/env python3
 
 """
-1. Async Comprehensions
+0. Async Generator
 """
 
 
-from typing import List
+import asyncio
+import random
 
 
-async_generator = __import__("0-async_generator").async_generator
-
-
-async def async_comprehension() -> List[float]:
+async def async_generator() -> float:
     """
-    Async function that returns a list of floats.
-
-    This function uses an async generator to generate
-    a sequence of floats, and then uses an async
-    comprehension to transform the generated sequence
-    into a list of floats.
-
-    Returns:
-      A list of floats generated from the async generator.
-
+    The async_generator function asynchronously
+    yields random numbers between 0 and 10 after
+    a 1-second delay for each iteration.
     """
-    return [i async for i in async_generator()]
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
